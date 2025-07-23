bdbaaf57e4e6b5688bf1384f6ffa69e2dfa5c241
Shubhamcs074 <shubhamsaini1027bagar@gmail.com>
2025-07-23 22:02:06 +0530

    ADD them

--- day3/problem1.cpp
+++ day3/problem1.cpp
@@ -1,24 +1,21 @@
-#include<iostream>
-using namespace std; 
-void searchInsert(int nums[], int k, int target);       
-int main (){
-    int nums[4] = {1, 3, 5, 6};
-    int k = 4;
-    int target;
-    cout << "Enter the target value:";
-    cin >> target;
-    searchInsert(nums, k, target);
+#include <vector>
+using namespace std;
 
-}
-void searchInsert(int nums[], int k, int target) {
-  if (target > nums[k-1]) {
-   cout << k;
-  } else {
-    for(int i = 0; i < k; i++){
-        if(nums[i] >= target){
-            cout << i;
-            return;
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.empty()) {
+            return 0;
         }
+
+        int k = 1;
+        for(int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        
+        return k;
     }
-  }
-}
+};
